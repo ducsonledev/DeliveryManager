3b945e4be100d91784c5821e8a9dc79d219d777c
SON <test@test.test>
2021-10-28 10:14:31 +0200

    Test Client for lieferando api

---
+++ TestClientLieferandoSandboxApi/TestClientLieferandoSandboxApi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClientLieferandoSandboxApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://sandbox-pull-posapi.takeaway.com/";
+            var httpClient = new HttpClient();
+            var byteArray = Encoding.ASCII.GetBytes("test-username-123:test-password-123");
+
+            httpClient.BaseAddress = new Uri(url);
+            httpClient.DefaultRequestHeaders.Add("Apikey", "abc123");
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(byteArray));
+
+            var response = await httpClient.GetAsync("1.0/orders/1234567");
+            Console.Write(response);
+        }
+    }
+}
