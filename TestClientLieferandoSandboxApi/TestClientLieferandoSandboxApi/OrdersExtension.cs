59901080888b4aee5378d8e7e5d1f27f64e5897e
SON <test@test.test>
2021-11-01 19:44:53 +0100

    Sending serialized JSON from LieferandoAPI to our server
    - extension method for serializing JSON for our server (please check
    comments in `OrdersExtension.cs`, lots of properties that are not
    provided by the LieferandoAPI or properties  that are not handled by
    our API server)
    - not sure about how to running script in background yet, have
    tried with thread sleeper in line 193, probably not best practice.

---
+++ TestClientLieferandoSandboxApi/TestClientLieferandoSandboxApi/OrdersExtension.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using static TestClientLieferandoSandboxApi.Program;
+
+namespace TestClientLieferandoSandboxApi
+{
+    public static class OrdersExtension
+    {
+        // Extension method for converting Lieferando's json string format for their orders
+        // to a format for our server.
+        public static List<OwnOrders> ToOwnOrder(this LieferandoOrders lieferandoOrders)
+        {
+            List<OwnOrders> listOfOwnOrders = new List<OwnOrders>();
+            foreach (var order in lieferandoOrders.orders)
+            {
+                List<ListProductOrder> listOfproducts = new List<ListProductOrder>();
+                foreach (var product in order.products)
+                {
+                    var newProduct = new ListProductOrder
+                    {
+                        status = "INPROGRESS", // what are other status?
+                        categoryDesc = product.category,
+                        categoryName = product.category,
+                        secondPrintAt = "EPSON TM-T82 Receipt", // not provided from Lieferando API
+                        productType = "PRODUCT", // not provided
+                        productName = product.name,
+                        productId = Int16.Parse(product.id),
+                        groupName = "Küche", // not provided
+                        productArticalNumber = "1", // not provided
+                        printAt = "PRINTKITCHEN", // not provided
+                        tableId = 0, // not provided
+                        signatureCount = -1, // not provided
+                        orderId = Int32.Parse(order.id),
+                        categoryId = 2, // not provided
+                        groupId = 1, // not provided
+                        index = 0, // not provided
+                        isLine = false, // not provided
+                        editIndex = 0, // not provided
+                        productPrice = product.price,
+                        discountRate = 0.0, // only from order.discounts but in an array
+                        quantity = product.count,
+                        discount = 0.0, // only from order.discounts but in an array
+                        makedKitchen = 0, // not provided
+                        numCus = 1, // not provided
+                        border = 0, // not provided, what is it for?
+                        money = product.price,
+                        tablePos = 0, // not provided, delivery?
+                        tax = 0.23, // not provided
+                        taxRate = 0.07, // standard delivery tax
+                        totalDiscount = 0.0, // only from as an array from order.discounts which is in fact
+                                             // totalDiscount but for full order not just one product
+                        totalMoney = product.price, // why so many different price tags?
+                        totalTax = 0.23, // not provided
+                        transferToKitchen = 0, // not provided
+                        allowChangeTax = 1 // not provided
+                        // missing information provided by Lieferando API that is not used in for our API:
+                        // - (not mandatory) sideDishes (and its id), remark
+                    };
+                    listOfproducts.Add(newProduct);
+                }
+
+                var newOrder = new OwnOrders
+                {
+                    action = "automaticOrder", // not provided
+                    signature = order.orderKey,
+                    time = "26/10/2021 21:15:40",
+                    user = "1",
+                    zdata = new Zdata
+                    {
+                        customer = {
+                            customerName = order.customer.name,
+                            customerCompany = order.customer.companyName,
+                            customerPhone = order.customer.phoneNumber,
+                            customerCode = order.customer.postalCode,
+                            customerCity =order.customer.city,
+                            customerAddress = order.customer.street,
+                            customerMail = "tungnm.ptit@gmail.com",
+                            customerType = "ECONOMIC",
+                            id = 1
+                            // missing information provided by Lieferando API that is not used in for our API:
+                            // - (not mandatory) extraAddressInfo
+                        },
+                        listProductOrders = listOfproducts,
+                        waiter = "1" // not provided
+                    },
+                    // missing information provided by Lieferando API that is not used in for our API:
+                    // - (not mandatory) remark
+                };
+
+                listOfOwnOrders.Add(newOrder);
+            }
+
+
+            return listOfOwnOrders;
+        }
+    }
+}
--- TestClientLieferandoSandboxApi/TestClientLieferandoSandboxApi/Program.cs
+++ TestClientLieferandoSandboxApi/TestClientLieferandoSandboxApi/Program.cs
@@ -1,24 +1,198 @@
-﻿using System;
-using System.Net.Http;
+﻿using RestSharp;
+using System;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace TestClientLieferandoSandboxApi
 {
-    class Program
+
+    public class Program
     {
-        static async Task Main(string[] args)
+        //public class Order
+        //{
+        //    [JsonProperty("id")]
+        //    public string Id { get; set; }
+        //    [JsonProperty("key")]
+        //    public string Key { get; set; }
+        //}
+        //  
+
+        // Model for JSON from Cash System API
+        public class newCustomer
         {
-            var url = "https://sandbox-pull-posapi.takeaway.com/";
-            var httpClient = new HttpClient();
+            public string customerName { get; set; }
+            public string customerCompany { get; set; }
+            public string customerPostCode { get; set; }
+            public string customerCity { get; set; }
+            public string customerAddress { get; set; }
+            public string customerTaxCode { get; set; }
+            public string customerPhone { get; set; }
+            public string customerMail { get; set; }
+            public string customerCode { get; set; }
+            public string customerType { get; set; }
+            public int id { get; set; }
+        }
+
+        public class ListProductOrder
+        {
+            public string status { get; set; }
+            public string categoryDesc { get; set; }
+            public string categoryName { get; set; }
+            public string secondPrintAt { get; set; }
+            public string productType { get; set; }
+            public string productName { get; set; }
+            public int productId { get; set; }
+            public string groupName { get; set; }
+            public string productArticalNumber { get; set; }
+            public string printAt { get; set; }
+            public int tableId { get; set; }
+            public int signatureCount { get; set; }
+            public int orderId { get; set; }
+            public int categoryId { get; set; }
+            public int groupId { get; set; }
+            public int index { get; set; }
+            public bool isLine { get; set; }
+            public int editIndex { get; set; }
+            public double productPrice { get; set; }
+            public double discountRate { get; set; }
+            public int quantity { get; set; }
+            public double discount { get; set; }
+            public int makedKitchen { get; set; }
+            public int numCus { get; set; }
+            public int border { get; set; }
+            public double money { get; set; }
+            public int tablePos { get; set; }
+            public double tax { get; set; }
+            public double taxRate { get; set; }
+            public double totalDiscount { get; set; }
+            public double totalMoney { get; set; }
+            public double totalTax { get; set; }
+            public int transferToKitchen { get; set; }
+            public int allowChangeTax { get; set; }
+        }
+
+        public class Zdata
+        {
+            public newCustomer customer { get; set; }
+            public List<ListProductOrder> listProductOrders { get; set; }
+            public string waiter { get; set; }
+        }
+
+        public class OwnOrders
+        {
+            public string action { get; set; }
+            public string signature { get; set; }
+            public string time { get; set; }
+            public string user { get; set; }
+            public Zdata zdata { get; set; }
+        }
+
+        // Model for JSON from Lieferando API
+        public class SideDish
+        {
+            public int price { get; set; }
+            public string name { get; set; }
+            public int count { get; set; }
+            public string id { get; set; }
+        }
+
+        public class Product
+        {
+            public double price { get; set; }
+            public string name { get; set; }
+            public int count { get; set; }
+            public string remark { get; set; }
+            public string id { get; set; }
+            public string category { get; set; }
+            public List<SideDish> sideDishes { get; set; }
+        }
+
+        public class Discount
+        {
+            public double price { get; set; }
+            public string name { get; set; }
+            public int count { get; set; }
+        }
+
+        public class Customer
+        {
+            public string phoneNumber { get; set; }
+            public string streetNumber { get; set; }
+            public string city { get; set; }
+            public string street { get; set; }
+            public string companyName { get; set; }
+            public string postalCode { get; set; }
+            public string name { get; set; }
+            public string extraAddressInfo { get; set; }
+        }
+
+        public class Order
+        {
+            public string orderKey { get; set; }
+            public object requestedDeliveryTime { get; set; }
+            public string orderType { get; set; }
+            public double totalPrice { get; set; }
+            public string remark { get; set; }
+            public string restaurantId { get; set; }
+            public string version { get; set; }
+            public string platform { get; set; }
+            public List<Product> products { get; set; }
+            public bool isPaid { get; set; }
+            public int deliveryCosts { get; set; }
+            public List<Discount> discounts { get; set; }
+            public string publicReference { get; set; }
+            public string courier { get; set; }
+            public double totalDiscount { get; set; }
+            public string paymentMethod { get; set; }
+            public string id { get; set; }
+            public DateTime orderDate { get; set; }
+            public Customer customer { get; set; }
+        }
+
+        public class LieferandoOrders
+        {
+            public List<Order> orders { get; set; }
+        }
+
+        //public class OrdersList
+        //{
+        //    [JsonProperty("orders")]
+        //    public string List<Order> Orders { get; set; }
+        //}
+
+        static void Main(string[] args)
+        {
+            TimeSpan interval = new TimeSpan(0, 1, 0);
+
             var byteArray = Encoding.ASCII.GetBytes("test-username-123:test-password-123");
+            var clientGet = new RestClient("https://sandbox-pull-posapi.takeaway.com/1.0/orders/1234567");
+            clientGet.Timeout = -1;
+            var requestGetOrd = new RestRequest(Method.GET);
+            requestGetOrd.AddHeader("content-type", "application/json");
+            requestGetOrd.AddHeader("Apikey", "abc123");
+            requestGetOrd.AddHeader("Authorization", "Basic " + Convert.ToBase64String(byteArray));
 
-            httpClient.BaseAddress = new Uri(url);
-            httpClient.DefaultRequestHeaders.Add("Apikey", "abc123");
-            httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(byteArray));
+            var clientPost = new RestClient("<server-base-adresse>:13000"); // TODO: Please add server adresse with Port 13000.
+            var requestPostOrd = new RestRequest(Method.POST);
+            while (true)
+            {
+                IRestResponse responseGet = clientGet.Execute(requestGetOrd);
+                Console.WriteLine(responseGet.Content);
 
-            var response = await httpClient.GetAsync("1.0/orders/1234567");
-            Console.Write(response);
+                var newLieferandoOrders = JsonConvert.DeserializeObject<LieferandoOrders>(responseGet.Content);
+                var ownOrders = newLieferandoOrders.ToOwnOrder();
+                var newOwnOrders = JsonConvert.SerializeObject(ownOrders, Formatting.Indented);
+
+                foreach (var newOwnOrder in newOwnOrders)
+                {
+                    IRestResponse responsePost = clientPost.Execute(requestPostOrd.AddJsonBody(newOwnOrder));
+                    Console.WriteLine(responsePost.Content);
+                }
+
+                Thread.Sleep(interval);
+            }
         }
     }
 }
